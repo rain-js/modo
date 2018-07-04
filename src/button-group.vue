e28599a235a74d51d243fb7084b3445ef6f7a7c9
rain-js <unique_ritchie@163.com>
2018-07-04 15:38:31 +0800

    :apple:组件化 button-group.vue

---
+++ src/button-group.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+    .button-group {
+        display: inline-flex;
+        > * {
+            border-radius: 0;
+            margin-right: -1px;
+        }
+        > *:hover {
+            z-index: 1;
+        }
+        :first-child {
+            border-top-left-radius: var(--border-radius);
+            border-bottom-left-radius: var(--border-radius);
+        }
+        :last-child {
+            border-top-right-radius: var(--border-radius);
+            border-bottom-right-radius: var(--border-radius);
+        }
+    }
+</style>
